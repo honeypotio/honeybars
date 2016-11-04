fb2472a3f89e28c8db2241993f702c641670836c
frederikbohn <fb@honeypot.io>
2016-11-04 12:21:33 +0100

    fixed https from http

--- rhyming.js
+++ rhyming.js
@@ -1,6 +1,6 @@
 var word = prompt('What do you wanna rhyme, big fella?');
 
-fetch("http://api.wordnik.com/v4/word.json/" + word + "/relatedWords?&useCanonical=false&relationshipTypes=rhyme&limitPerRelationshipType=10&api_key=7c91c9072060dec2af00a04a7ab0fa6d0530cf8a943cafa90")
+fetch("https://api.wordnik.com/v4/word.json/" + word + "/relatedWords?&useCanonical=false&relationshipTypes=rhyme&limitPerRelationshipType=10&api_key=7c91c9072060dec2af00a04a7ab0fa6d0530cf8a943cafa90")
   .then(function(response){
     return response.json();
   })
