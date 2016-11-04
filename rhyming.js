2305e26b7e67c1ecdf121434ca50348e38857153
frederikbohn <fb@honeypot.io>
2016-11-04 12:19:36 +0100

    fixed a typo...

--- rhyming.js
+++ rhyming.js
@@ -1,4 +1,4 @@
-var world = prompt('What do you wanna rhyme, big fella?');
+var word = prompt('What do you wanna rhyme, big fella?');
 
 fetch("http://api.wordnik.com/v4/word.json/" + word + "/relatedWords?&useCanonical=false&relationshipTypes=rhyme&limitPerRelationshipType=10&api_key=7c91c9072060dec2af00a04a7ab0fa6d0530cf8a943cafa90")
   .then(function(response){
