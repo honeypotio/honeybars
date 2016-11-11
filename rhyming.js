d80448e8409aaefc8ea55e7d040c1fd5927ea5f2
ktaner <kt@honeypot.io>
2016-11-11 11:57:55 +0100

    changed prompt to form

--- rhyming.js
+++ rhyming.js
@@ -1,10 +1,21 @@
-var word = prompt('What do you wanna rhyme, big fella?');
+var form = document.getElementById('form');
 
-fetch("https://api.wordnik.com/v4/word.json/" + word + "/relatedWords?&useCanonical=false&relationshipTypes=rhyme&limitPerRelationshipType=10&api_key=7c91c9072060dec2af00a04a7ab0fa6d0530cf8a943cafa90")
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(jsonResponse){
-    var rhymingWords = jsonResponse[0].words.join(' ');
-    document.getElementById('wordList').textContent = rhymingWords;
-  });
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); //prevents page from e.g. refreshing
+
+  var word = this.word.value;
+  getRhymingWords(word);
+})
+
+//var word = prompt('What do you wanna rhyme, big fella?');
+
+function getRhymingWords(word) {
+  fetch("https://api.wordnik.com/v4/word.json/" + word + "/relatedWords?&useCanonical=false&relationshipTypes=rhyme&limitPerRelationshipType=10&api_key=7c91c9072060dec2af00a04a7ab0fa6d0530cf8a943cafa90")
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(jsonResponse){
+      var rhymingWords = jsonResponse[0].words.join(' ');
+      document.getElementById('wordList').textContent = rhymingWords;
+    });
+}
